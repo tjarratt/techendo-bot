82e7fdca0201cea86ff87af1496c8a0462f88c70
Tim Jarratt <tjarratt@gmail.com>
2013-09-08 21:57:34 -0700

    Fix error
    I knew I caught this at some point, but I had thought it was fixed.

--- actions/chat_response.rb
+++ actions/chat_response.rb
@@ -6,7 +6,7 @@ class ChatResponseAction < BaseAction
   end
 
   def self._action(m)
-    if message.match(/you there, techendo\-pal\?/)
+    if m.message.match(/you there, techendo\-pal\?/)
       m.reply "Yes. I believe so, #{m.user.name}. I visualize a time when we will be to robots what dogs are to humans, and I'm rooting for the machines."
     else
       m.reply "You know I can hear you talking about me, right?"
